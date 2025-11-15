93c3a7d232a8a1f58ee2c7f0f29cb3e6b2712f79
Abhinav Singh <abhinavsingh90909@gmail.com>
2025-11-15 19:09:02 +0530

    Add files via upload

---
+++ config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2477537249-ea165",
+  "appId": "1:770687453248:web:e097ed5236bbb79f2bb7a5",
+  "apiKey": "AIzaSyAtPaeQia4ir_B3CMGTuBdTpjIWJiKhLRg",
+  "authDomain": "studio-2477537249-ea165.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "770687453248"
+};
